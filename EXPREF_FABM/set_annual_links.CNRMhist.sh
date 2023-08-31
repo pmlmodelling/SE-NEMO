1e51ee452fddc1381c3ec39c263b61874e06728b
Yuri Artioli <yuri@pml.ac.uk>
2023-08-31 17:47:49 +0100

    changes in set-up to align filenames and format. RUNS

--- EXPREF_FABM/set_annual_links.CNRMhist.sh
+++ EXPREF_FABM/set_annual_links.CNRMhist.sh
@@ -27,14 +27,14 @@ ln -sf  $INDIR_Ndep/eORCA025_r015-r010_007_004v2_N_dep_ISIMIP_y${ya}.nc $RUNPATH
 
 ln -sf  $INDIR_Fedep/eORCA025_r015-r010_007_004v2_Fe_dep_GESAMP.nc $RUNPATH/eORCA025_Fe_dep_GESAMP.nc
 
-ln -sf $INDIR_RIVERS/ORCA025_rivers_AG_BGC_dep_spread_y${yn}.nc ${RUNPATH}/JRA_BGC_y${yn}.nc
-ln -sf $INDIR_RIVERS/ORCA025_rivers_AG_BGC_dep_spread_y${ya}.nc ${RUNPATH}/JRA_BGC_y${ya}.nc
-ln -sf $INDIR_RIVERS/ORCA025_rivers_AG_BGC_dep_spread_y${yb}.nc ${RUNPATH}/JRA_BGC_y${yb}.nc
-ln -sf $INDIR_RIVERS/runoff_socoefr_v2.nc  ./runoff_socoefr_v2.nc
+ln -sf $INDIR_RIVERS/CNRM_river_BGC_y${yn}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${yn}.nc
+ln -sf $INDIR_RIVERS/CNRM_river_BGC_y${ya}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${ya}.nc
+ln -sf $INDIR_RIVERS/CNRM_river_BGC_y${yb}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${yb}.nc
+ln -sf $INDIR_RIVERS/runoff_socoefr_v2.nc  ./RIVERS/runoff_socoefr_v2.nc
 
 
-ln -sf  $INDIR_pCO2a/eORCA025_r015-r010_007_004v2_pCO2a_global_y${yn}.nc $RUNPATH/eORCA025_pCO2a_global_y${yn}.nc
-ln -sf  $INDIR_pCO2a/eORCA025_r015-r010_007_004v2_pCO2a_global_y${yb}.nc $RUNPATH/eORCA025_pCO2a_global_y${yb}.nc
-ln -sf  $INDIR_pCO2a/eORCA025_r015-r010_007_004v2_pCO2a_global_y${ya}.nc $RUNPATH/eORCA025_pCO2a_global_y${ya}.nc
+ln -sf  $INDIR_pCO2a/eORCA025_v2_pCO2a_global_y${yn}.nc $RUNPATH/eORCA025_pCO2a_global_y${yn}.nc
+ln -sf  $INDIR_pCO2a/eORCA025_v2_pCO2a_global_y${yb}.nc $RUNPATH/eORCA025_pCO2a_global_y${yb}.nc
+ln -sf  $INDIR_pCO2a/eORCA025_v2_pCO2a_global_y${ya}.nc $RUNPATH/eORCA025_pCO2a_global_y${ya}.nc
 
 ln -sf  $INDIR_ADY/eORCA025_r015-r010_007_004v2_ady.nc ${RUNPATH}/eORCA025-CCI-ady-broadband-climatology.nc
