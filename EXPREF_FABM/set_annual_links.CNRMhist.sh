610992b0bce7dba46202e9a73a59c81e000ea5bb
Giovanni Galli <gig@pml.ac.uk>
2023-10-06 15:21:35 +0100

    fixed runscript for CNRMssp370 run, also relevent fabm_input and set_annual_links

--- EXPREF_FABM/set_annual_links.CNRMhist.sh
+++ EXPREF_FABM/set_annual_links.CNRMhist.sh
@@ -28,10 +28,10 @@ ln -sf  $INDIR_Ndep/eORCA025_r015-r010_007_004v2_N_dep_ISIMIP_y${ya}.nc $RUNPATH
 
 ln -sf  $INDIR_Fedep/eORCA025_r015-r010_007_004v2_Fe_dep_GESAMP.nc $RUNPATH/eORCA025_Fe_dep_GESAMP.nc
 
-ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${yn}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${yn}.nc
-ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${ya}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${ya}.nc
-ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${yb}.nc ${RUNPATH}/RIVERS/CNRMhist_BGC_y${yb}.nc
-ln -sf $INDIR_RIVERS/runoff_socoefr_v2.nc  ./RIVERS/runoff_socoefr_v2.nc
+ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${yn}.nc ${RUNPATH}/RIVERS/river_BGC_y${yn}.nc
+ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${ya}.nc ${RUNPATH}/RIVERS/river_BGC_y${ya}.nc
+ln -sf $INDIR_RIVERS/CNRM_hist_river_BGC_y${yb}.nc ${RUNPATH}/RIVERS/river_BGC_y${yb}.nc
+ln -sf $INDIR_RIVERS/runoff_socoefr_v2.nc  ${RUNPATH}/RIVERS/runoff_socoefr_v2.nc
 
 
 ln -sf  $INDIR_pCO2a/eORCA025_r015-r010_007_004v2_pCO2a_global_CMIP6_hist_y${yn}.nc $RUNPATH/eORCA025_pCO2a_global_y${yn}.nc
--- EXPREF_FABM/set_annual_links.CNRMssp370.sh
+++ EXPREF_FABM/set_annual_links.CNRMssp370.sh
@@ -9,6 +9,7 @@ INDIR_Fedep=/work/n01/shared/yuti/se-ORCA025_Fedep/
 INDIR_pCO2a=/work/n01/shared/yuti/se-ORCA025_pCO2a/CMIP6/
 INDIR_ADY=/work/n01/shared/yuti/se-ORCA025_ADY_clim/
 INDIR_RIVERS=/work/n01/shared/yuti/se-ORCA025_CNRM_SSP370/
+INDIR_RIVERS_hist=/work/n01/shared/yuti/se-ORCA025_CNRM_hist/
 
 
 RUNPATH=$PWD
@@ -28,10 +29,10 @@ ln -sf  $INDIR_Ndep/eORCA025_r015-r010_007_004v2_N_dep_ISIMIP_ssp370_y${ya}.nc $
 ln -sf  $INDIR_Fedep/eORCA025_r015-r010_007_004v2_Fe_dep_GESAMP.nc $RUNPATH/eORCA025_Fe_dep_GESAMP.nc
 
 # PLEASE NOTE the name of the symolic link file is CURRENTLY CNRMhist, because it was easier due to ARCHER2 maintenance, however best to change it
-ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${yn}.nc ${RUNPATH}/CNRMhist_BGC_y${yn}.nc
-ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${ya}.nc ${RUNPATH}/CNRMhist_BGC_y${ya}.nc
-ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${yb}.nc ${RUNPATH}/CNRMhist_BGC_y${yb}.nc
-ln -sf $INDIR_RIVERS/runoff_socoefr_v2.nc  ./runoff_socoefr_v2.nc
+ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${yn}.nc ${RUNPATH}/RIVERS/river_BGC_y${yn}.nc
+ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${ya}.nc ${RUNPATH}/RIVERS/river_BGC_y${ya}.nc
+ln -sf $INDIR_RIVERS/CNRM_ssp370_river_BGC_y${yb}.nc ${RUNPATH}/RIVERS/river_BGC_y${yb}.nc
+ln -sf $INDIR_RIVERS_hist/runoff_socoefr_v2.nc  ${RUNPATH}/RIVERS/runoff_socoefr_v2.nc
 
 
 ln -sf  $INDIR_pCO2a/eORCA025_r015-r010_007_004v2_pCO2a_global_CMIP6_SSP370_y${yn}.nc $RUNPATH/eORCA025_pCO2a_global_y${yn}.nc
